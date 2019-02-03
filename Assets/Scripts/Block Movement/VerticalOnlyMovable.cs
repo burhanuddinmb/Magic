6e12ca7919f62b00b8e83f64c714b965ff682228
Burhanuddin Beawerwala <bxb5733@rit.edu>
2019-02-03 15:59:14 -0500

    All scripts for mechanics added for now.

---
+++ Assets/Scripts/Block Movement/VerticalOnlyMovable.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalOnlyMovable : MonoBehaviour
+{
+    Vector3 futurePosition;
+
+    Vector2 initialTouchSpace;
+    Vector2 deltaTouchSpace;
+
+    bool isTouchActive;
+    bool isObjectSelected;
+    bool isPlayerConnected;
+    bool deselectObject;
+
+    [SerializeField] GameObject player;
+
+    [SerializeField] float maxY;
+    [SerializeField] float minY;
+
+    float startTime;
+    float eachFrameTimeVariable;
+
+    float movementSpeed;
+
+    void Start()
+    {
+        movementSpeed = 5.0f;
+        isPlayerConnected = false;
+        deselectObject = true;
+    }
+
+    void Update()
+    {
+        HandleTouch();
+
+        if (isTouchActive && isObjectSelected)
+        {
+            isTouchActive = false;
+
+            futurePosition = transform.localPosition;
+            futurePosition.x = transform.localPosition.x - (deltaTouchSpace.x * Time.deltaTime * movementSpeed);
+            futurePosition.x = Mathf.Clamp(futurePosition.x, minY, maxY);
+
+            transform.localPosition = futurePosition;
+            isObjectSelected = !deselectObject;
+        }
+    }
+
+    void HandleTouch()
+    {
+        if (Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit, 100.0f))
+                {
+                    if (hit.transform.tag == "HorizontalTouchMove" && hit.transform == transform)
+                    {
+                        isObjectSelected = true;
+                    }
+                }
+                startTime = Time.time;
+                eachFrameTimeVariable = startTime;
+                initialTouchSpace = touch.position;
+                deselectObject = false;
+            }
+            else if (touch.phase == TouchPhase.Moved)
+            {
+                CheckChangeInTouch(touch);
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                CheckChangeInTouch(touch);
+                deselectObject = true;
+            }
+
+            if (!isTouchActive && isObjectSelected)
+            {
+                float timeChange = Time.time - startTime;
+
+                if (timeChange > 0.1f)
+                {
+                    isTouchActive = true;
+                    //player.GetComponent<PlayerController>().StopPlayer();
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.parent = transform;
+            isPlayerConnected = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.parent = transform.parent;
+            isPlayerConnected = false;
+        }
+    }
+
+    void CheckChangeInTouch(Touch touch)
+    {
+        if (isObjectSelected)
+        {
+            deltaTouchSpace = initialTouchSpace - touch.position;
+            initialTouchSpace = touch.position;
+        }
+    }
+}
