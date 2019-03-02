2d19a96bb202ba75979b4f0a7a53bc6204953f35
Burhanuddin Beawerwala <bxb5733@rit.edu>
2019-03-02 12:06:17 -0500

    Z movement added

---
+++ Assets/Scripts/Block Movement/HorizontalOnlyZMovable.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalOnlyZMovable : MonoBehaviour
+{
+    Vector3 futurePosition;
+    Node node;
+    AccessibleNodes accessibleNodes;
+
+    Vector2 initialTouchSpace;
+    Vector2 deltaTouchSpace;
+
+    bool isTouchActive;
+    bool isObjectSelected;
+    bool deselectObject;
+
+    [SerializeField] float maxZ;
+    [SerializeField] float minZ;
+
+    float startTime;
+    float eachFrameTimeVariable;
+
+    float movementSpeed;
+
+    void Start()
+    {
+        node = GetComponent<Node>();
+        accessibleNodes = GetComponent<AccessibleNodes>();
+        movementSpeed = 5.0f;
+        deselectObject = true;
+    }
+
+    void Update()
+    {
+        HandleTouch();
+        CheckForAccessibleNodes();
+        if (isTouchActive && isObjectSelected)
+        {
+            isTouchActive = false;
+
+            futurePosition = transform.localPosition;
+            futurePosition.z = transform.localPosition.z - (deltaTouchSpace.z * Time.deltaTime * movementSpeed);
+            futurePosition.z = Mathf.Clamp(futurePosition.z, minZ, maxZ);
+
+            transform.localPosition = futurePosition;
+            isObjectSelected = !deselectObject;
+        }
+    }
+
+    void CheckForAccessibleNodes()
+    {
+        if (Mathf.Abs(transform.localPosition.z - node.gridZ) >= 1)
+        {
+            node.gridZ = Mathf.RoundToInt(transform.localPosition.z);
+            ReAdjustNodes();
+        }
+    }
+
+    void ReAdjustNodes()
+    {
+        foreach (var connectingNode in accessibleNodes.connectingNodes)
+        {
+            AccessibleNodes accNodes = connectingNode.transform.GetComponent<AccessibleNodes>();
+            for (int i = 0; i < accNodes.connectingNodes.Count; i++)
+            {
+                accNodes.connectingNodes.Remove(transform.GetComponent<Node>());
+            }
+        }
+        accessibleNodes.CalculateConnectingNodes();
+
+        foreach (var connectingNode in accessibleNodes.connectingNodes)
+        {
+            connectingNode.transform.GetComponent<AccessibleNodes>().connectingNodes.Add(transform.GetComponent<Node>());
+        }
+    }
+
+    void HandleTouch()
+    {
+        if (Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit, 100.0f))
+                {
+                    if (hit.transform.tag == "HorizontalTouchMove" && hit.transform == transform)
+                    {
+                        isObjectSelected = true;
+                        Camera.main.GetComponent<CameraScript>().isAnythingImportantGoingOn = true;
+                    }
+                }
+                startTime = Time.time;
+                eachFrameTimeVariable = startTime;
+                initialTouchSpace = touch.position;
+                deselectObject = false;
+            }
+            else if (touch.phase == TouchPhase.Moved)
+            {
+                CheckChangeInTouch(touch);
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                CheckChangeInTouch(touch);
+                Camera.main.GetComponent<CameraScript>().isAnythingImportantGoingOn = false;
+                deselectObject = true;
+            }
+
+            if (!isTouchActive && isObjectSelected)
+            {
+                float timeChange = Time.time - startTime;
+
+                if (timeChange > 0.1f)
+                {
+                    isTouchActive = true;
+                    //player.GetComponent<PlayerController>().StopPlayer();
+                }
+            }
+        }
+    }
+
+    void CheckChangeInTouch(Touch touch)
+    {
+        if (isObjectSelected)
+        {
+            deltaTouchSpace = initialTouchSpace - touch.position;
+            initialTouchSpace = touch.position;
+        }
+    }
+}
