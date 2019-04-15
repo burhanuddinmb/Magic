7f3581b35ca19610c40a4179d8e94b29f6d9a8e4
Burhanuddin Beawerwala <bxb5733@rit.edu>
2019-04-15 19:27:58 -0400

    Touch decal changes

--- Assets/Scripts/Navigation/TouchDecal.cs
+++ Assets/Scripts/Navigation/TouchDecal.cs
@@ -8,8 +8,9 @@ public class TouchDecal : MonoBehaviour
     GameObject decalRef;
     Transform world;
 
-    public static Vector3 positionToSlap;
+    //public static Vector3 positionToSlap;
     public static bool recreateDecal;
+    public static Transform objectTransform;
 
     // Start is called before the first frame update
     void Start()
@@ -26,26 +27,36 @@ public class TouchDecal : MonoBehaviour
             ApplyDecal();
     }
 
-    void SlapDecal()
-    {
-        decalRef.transform.position = positionToSlap;
-    }
-
     public static void AddTouchAnimation(Transform tr)
     {
-        Vector3 newPos = tr.localPosition;
-        
-        newPos.x += tr.localScale.x / 2;
-        newPos.y += tr.localScale.y + 0.01f;
-        newPos.z -= tr.localScale.z / 2;
-        
-        positionToSlap = newPos;
+        //Type1
+        //Vector3 newPos = tr.localPosition;
+        //
+        //newPos.x += tr.localScale.x / 2;
+        //newPos.y += tr.localScale.y + 0.01f;
+        //newPos.z -= tr.localScale.z / 2;
+        //positionToSlap = newPos;
+
+        //Type2
+        objectTransform = tr;
+
         recreateDecal = true;
+       
     }
 
     void ApplyDecal()
     {
-        decalRef.transform.localPosition = positionToSlap;
+        //Type1
+        //decalRef.transform.localPosition = positionToSlap;
+
+        //Type2
+        Vector3 newPos = Vector3.zero;
+        newPos.x += objectTransform.localScale.x / 4;
+        newPos.y += objectTransform.localScale.y/2 + 0.01f;
+        newPos.z -= objectTransform.localScale.z / 4;
+        decalRef.transform.parent = objectTransform;
+        decalRef.transform.localPosition = newPos;
+
         //decalRef.SetActive(true);
         decalRef.GetComponent<ParticleSystem>().Stop();
         decalRef.GetComponent<ParticleSystem>().Play();
